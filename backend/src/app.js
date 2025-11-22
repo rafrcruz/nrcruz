044a33847d21388b31f1a07cf40b7b910427f9cc
Rafael Cruz <rafrcruz@gmail.com>
2025-11-22 12:20:39 -0300

    Enable Sentry tracing for Express requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
 
-const { initSentry } = require('./config/sentry');
+const { initSentry, getRequestHandler, getErrorHandler } = require('./config/sentry');
 const { config } = require('./config/env');
 const routes = require('./routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { securityHeaders } = require('./middlewares/securityHeaders');
 const { rateLimiter, userAgentFilter } = require('./middlewares/trafficControl');
 
-initSentry();
-
 const app = express();
+
+// Inicializa o Sentry com tracing; mantém o app funcional mesmo sem DSN.
+initSentry(app);
 
 app.disable('x-powered-by');
 
@@ -38,9 +39,19 @@ app.use(rateLimiter);
 app.use(compression());
 app.use(express.json());
 
+const sentryRequestHandler = getRequestHandler();
+if (sentryRequestHandler) {
+  app.use(sentryRequestHandler);
+}
+
 app.use(routes);
 
 // Global error handler should be registered after routes
+const sentryErrorHandler = getErrorHandler();
+if (sentryErrorHandler) {
+  // Captura exceções para o Sentry e segue o fluxo de erros existente.
+  app.use(sentryErrorHandler);
+}
 app.use(errorHandler);
 
 module.exports = { app };
